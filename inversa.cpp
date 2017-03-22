e4dc5afb2b5f192189f2f6b2f3cd8ff5c9933865
Pipe0116 <jufvasquezri@unal.edu.co>
2017-03-21 23:35:14 -0500

    inversa, determinante y verificacion inversa

---
+++ inversa.cpp
@@ -0,0 +1,17 @@
+// MATRIZ INVERSA Y DETERMINANTE
+
+#include <iostream>
+#include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+int main()
+{
+   Matrix3f A;
+   A << 1, 2, 1,
+        2, 1, 0,
+        -1, 1, 2;
+   cout << "Here is the matrix A:\n" << A << endl;
+   cout << "The determinant of A is " << A.determinant() << endl;
+   cout << "The inverse of A is:\n" << A.inverse() << endl;
+   cout <<  "A*A-1 = :\n " << A*(A.inverse()) << endl;
+}
